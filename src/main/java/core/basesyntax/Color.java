21461be2bfa34e55f608dead0dea5c3d1a179810
Maksym <pozetiv13@gmail.com>
2026-02-13 17:05:26 +0200

    MaksymGorodnychyi

--- src/main/java/core/basesyntax/Color.java
+++ src/main/java/core/basesyntax/Color.java
@@ -4,7 +4,7 @@ public enum Color {
     RED,
     BLUE,
     GREEN,
-    YELLOW;
+    YELLOW
 
 }
 
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private ColorSupplier colorSupplier = new ColorSupplier();
-    private Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Ball getRandomBall() {
         Color randomColor = colorSupplier.getRandomColor();
